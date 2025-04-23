bf35df99e4f907bf4af7ef48dee31f3e7d2f4833
Anshulstark44 <lalaram5105@gmail.com>
2025-04-23 08:27:08 +0530

    add feature of persistReducer

--- client/src/apis/authApi.js
+++ client/src/apis/authApi.js
@@ -5,11 +5,6 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000
 export const signupUser = async (userData) => {
   try {
     const response = await axios.post(`${API_BASE_URL}/users/auth/signup`, userData);
-    
-    const firebaseUID = response.data?.firebaseUID;
-    if (firebaseUID) {
-      localStorage.setItem('firebaseUID', firebaseUID); // ✅ Save UID in localStorage
-    }
 
     return response.data;
   } catch (error) {
@@ -23,10 +18,6 @@ export const loginUser = async (firebaseUID) => {
     const response = await axios.get(`${API_BASE_URL}/users/auth/login/${firebaseUID}`);
     console.log(response);
     
-    if (firebaseUID) {
-      localStorage.setItem('firebaseUID', firebaseUID); // ✅ Save UID in localStorage
-    }
-
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message || "Login failed");
